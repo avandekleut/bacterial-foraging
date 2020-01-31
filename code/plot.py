adc14086e6f900d80b310ca58380c1b17a881252
Alex Van de Kleut <avandekleut@gmail.com>
2020-01-31 12:39:54 -0500

    added reproduction

--- code/plot.py
+++ code/plot.py
@@ -1,5 +1,5 @@
 import matplotlib
-matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['mathtext.fontset'] = 'stix' # set fonts to latex fonts
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import LightSource
@@ -8,7 +8,6 @@ from matplotlib import cm
 from matplotlib import animation
 import matplotlib.pyplot as plt
 import numpy as np
-np.random.seed(17)
 
 def plot_surface(J,  filename=None, rng=(-10, 10), num=100, cm=cm.rainbow):
     """
@@ -40,6 +39,7 @@ def plot_surface(J,  filename=None, rng=(-10, 10), num=100, cm=cm.rainbow):
         plt.show()
     else:
         plt.savefig(filename)
+    return ax
 
 def plot_J_cc(title=None, filename=None, S=3, rng=(-10, 10), num=100, cm=cm.rainbow, d_attract=0.1, w_attract=0.2, h_repellant=0.1, w_repellant=10):
     """
@@ -56,6 +56,7 @@ def plot_J_cc(title=None, filename=None, S=3, rng=(-10, 10), num=100, cm=cm.rain
     w_repellant: float > 0      reciprocal width of repellant
     Default values chosen to replicate plots from paper.
     """
+    np.random.seed(17) # for reproducability
     x = np.linspace(*rng, num=num)
     y = np.linspace(*rng, num=num)
     X, Y = np.meshgrid(x, y)
@@ -88,11 +89,12 @@ def plot_J_cc(title=None, filename=None, S=3, rng=(-10, 10), num=100, cm=cm.rain
         plt.show()
     else:
         plt.savefig(filename)
+    return ax
 
 def plot_J(J_histories, title=None, filename=None):
     """
     Plots the values of J achieved during optimization.
-    J_histories: iterable of iterables of floats
+    J_histories: np.ndarray(S, N_c)
     title: str                  title of plot
     filename: str               filename to save as
     """
@@ -108,12 +110,40 @@ def plot_J(J_histories, title=None, filename=None):
         plt.show()
     else:
         plt.savefig(f'{filename}_J.pdf')
+    return ax
+
+def plot_J_re(J_histories, title=None, filename=None):
+    """
+    Plots the values of J achieved during optimization.
+    J_histories: np.ndarray(N_re, S, N_c)
+    title: str                  title of plot
+    filename: str               filename to save as
+    """
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.set_xlabel(r'Iteration ($j$)')
+    ax.set_ylabel(r'$J(\theta)$')
+    ax.set_title(title)
+    N_re = J_histories.shape[0]
+    N_c  = J_histories.shape[2]
+    colours = ['r', 'g', 'b', 'c']*N_re
+    for k in range(N_re):
+        generation = J_histories[k]
+        colour = colours[k]
+        for J_history in generation:
+            ax.plot(range(k*N_c, (k+1)*N_c), J_history, alpha=0.2, color=colour)
+    ax.text(0.5, 0.5, r'$J^*$ =' f'{np.min(J_histories):.2}', horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize='medium')
+    if filename is None:
+        plt.show()
+    else:
+        plt.savefig(f'{filename}_J.pdf')
+    return ax
 
 def plot_paths(J, theta_histories, title=None, filename=None, num=100, cm=cm.rainbow, ):
     """
     Plots the paths taken during optimization.
     J: np.ndarray -> float      function to minimize
-    theta_histories: iterable of iterables of 2D np.ndarrays
+    theta_histories: J_histories: np.ndarray(S, N_c, p)
     title: str                  title of plot
     filename: str               filename to save as
     num: int                    how many points to use for contour plot grid
@@ -131,13 +161,49 @@ def plot_paths(J, theta_histories, title=None, filename=None, num=100, cm=cm.rai
     contour = ax.contour(X, Y, Z, cmap=cm)
 
     for theta_history in theta_histories:
-        theta_history = np.asarray(theta_history).T
-        ax.plot(*theta_history, color='black', alpha=0.5)
+        ax.plot(*theta_history.T, color='black', alpha=0.5)
 
     if filename is None:
         plt.show()
     else:
         plt.savefig(f'{filename}_theta.pdf')
+    return ax
+
+def plot_paths_re(J, theta_histories, title=None, filename=None, num=100, cm=cm.rainbow, ):
+    """
+    Plots the paths taken during optimization.
+    J: np.ndarray -> float      function to minimize
+    theta_histories: J_histories: np.ndarray(S, N_c, p)
+    title: str                  title of plot
+    filename: str               filename to save as
+    num: int                    how many points to use for contour plot grid
+    cm: matplotlib.cm           colormap to use for plotting
+    """
+    rng = (np.min(theta_histories), np.max(theta_histories))
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.set_title(title)
+    x = np.linspace(*rng, num=num)
+    y = np.linspace(*rng, num=num)
+    X, Y = np.meshgrid(x, y)
+    theta = np.array([X, Y])
+    Z = np.apply_along_axis(J, 0, theta)
+    contour = ax.contour(X, Y, Z, cmap=cm)
+
+    N_re = theta_histories.shape[0]
+    N_c  = theta_histories.shape[2]
+    colours = ['r', 'g', 'b', 'c']*N_re
+    for k in range(N_re):
+        generation = theta_histories[k]
+        colour = colours[k]
+        for theta_history in generation:
+            ax.plot(*theta_history.T, alpha=0.5, color=colour)
+
+    if filename is None:
+        plt.show()
+    else:
+        plt.savefig(f'{filename}_theta.pdf')
+    return ax
 
 def evaluate_sb(J, title=None, filename=None, **simulation_args):
     """
@@ -148,6 +214,7 @@ def evaluate_sb(J, title=None, filename=None, **simulation_args):
     simulation_args: kwargs     keyword arguments to be passed to
                                 single_bacterium.simulate
     """
+    np.random.seed(17) # for reproducability
     from single_bacterium import simulate
     J_histories = []
     theta_histories = []
@@ -168,10 +235,26 @@ def evaluate_col(J, title=None, filename=None, **simulation_args):
     simulation_args: kwargs     keyword arguments to be passed to
                                 colony.simulate
     """
+    np.random.seed(17) # for reproducability
     from colony import simulate
     J_histories, theta_histories = simulate(J, **simulation_args)
     plot_J(J_histories, title=title, filename=filename)
     plot_paths(J, theta_histories, title=title, filename=filename)
+
+def evaluate_col_re(J, title=None, filename=None, **simulation_args):
+    """
+    Used to run and generate plots for colony-optimization.
+    J: np.ndarray -> float      function to minimize
+    title: str                  title of plot
+    filename: str               filename to save as
+    simulation_args: kwargs     keyword arguments to be passed to
+                                colony.simulate
+    """
+    np.random.seed(17) # for reproducability
+    from colony_reproduction import simulate
+    J_histories, theta_histories = simulate(J, **simulation_args)
+    plot_J_re(J_histories, title=title, filename=filename)
+    plot_paths_re(J, theta_histories, title=title, filename=filename)
 
 if __name__ == "__main__":
     """
@@ -234,6 +317,44 @@ if __name__ == "__main__":
     evaluate_col(rastrigin,
         title=r'$S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=100, $w_\mathregular{attract}$=0.01, $h_\mathregular{repellant}$=0.3, $w_\mathregular{repellant}$=0.01',
         filename='presentation/assets/rastrigin_colony_tuned',
+        S=10,
+        p=2,
+        c=0.1,
+        N_c=100,
+        rng=(-10,10),
+        d_attract=100,
+        w_attract=0.01,
+        h_repellant=0.4,
+        w_repellant=0.01)
+
+    plot_J_cc(title=r'$S$=3, $p$=2, $d_\mathregular{attract}$=0.1, $w_\mathregular{attract}$=0.2, $h_\mathregular{repellant}$=0.1, $w_\mathregular{repellant}$=10',
+        filename='presentation/assets/swarming_tuned.pdf',
+        S=3,
+        rng=(-10, 10),
+        num=100,
+        d_attract=100,
+        w_attract=0.01,
+        h_repellant=0.4,
+        w_repellant=0.01)
+
+    evaluate_col_re(sphere,
+        title=r'$N_{re} = 4$, $S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=0.1, $w_\mathregular{attract}$=0.2, $h_\mathregular{repellant}$=0.1, $w_\mathregular{repellant}$=10',
+        filename='presentation/assets/sphere_colony_re',
+        N_re=4,
+        S=10,
+        p=2,
+        c=0.1,
+        N_c=100,
+        rng=(-10,10),
+        d_attract=0.1,
+        w_attract=0.2,
+        h_repellant=0.1,
+        w_repellant=10)
+
+    evaluate_col_re(rastrigin,
+        title=r'$N_{re}=4, $$S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=100, $w_\mathregular{attract}$=0.01, $h_\mathregular{repellant}$=0.3, $w_\mathregular{repellant}$=0.01',
+        filename='presentation/assets/rastrigin_colony_re',
+        N_re=4,
         S=10,
         p=2,
         c=0.1,
--- code/single_bacterium.py
+++ code/single_bacterium.py
@@ -1,18 +1,19 @@
 import numpy as np
+np.random.seed(17)
 
 def simulate(J, p=2, c=0.1, N_c=100, rng=(-10,10)):
     theta = rng[0] + np.random.rand(p)*(rng[1] - rng[0])
 
-    J_history = []
-    theta_history = []
+    J_history = np.zeros((N_c,))
+    theta_history = np.zeros((N_c, p))
 
     for j in range(N_c):
         phi = np.random.uniform(low=-1, high=1, size=p)
         phi /= np.linalg.norm(phi)
         theta = theta + c*phi
 
-        theta_history.append(theta.copy())
-        J_history.append(J(theta))
+        theta_history[j] = theta.copy()
+        J_history[j] = J(theta)
 
         while J(theta + c*phi) < J(theta):
             theta = theta + c*phi
