b70397c68afce3e6c9e1ee0a47ec372720fa6433
Alex Van de Kleut <avandekleut@gmail.com>
2020-02-03 10:17:32 -0500

    removed extra files

--- code/plot.py
+++ code/plot.py
@@ -290,131 +290,197 @@ def evaluate_pso(J, title=None, filename=None, **simulation_args):
     plot_J(J_history, title=title, filename=filename)
     plot_paths(J, theta_history, title=title, filename=filename)
 
+def compare_all(J, num_seeds=30, title=None, filename=None, S=10, p=2, c=0.1, N_ed=2, p_ed=0.25, N_re=4, N_c=100, rng=(-5.12,5.12), d_attract=100, w_attract=0.01, h_repellant=0.4, w_repellant=0.01):
+    J_single_bacterium_min = []
+    J_colony_min = []
+    J_colony_re_min = []
+    J_colony_ed_min = []
+    J_random_min = []
+
+    for n in range(num_seeds):
+        print(n)
+
+        np.random.seed(n)
+        from single_bacterium import simulate as simulate_single_bacterium
+        J_single_bacterium = []
+        for i in range(N_ed*N_re*S):
+            J_history, theta_history = simulate_single_bacterium(J, N_c=N_c, rng=rng)
+            J_single_bacterium.append(np.min(J_history))
+        J_single_bacterium_min.append(np.min(J_single_bacterium))
+
+        np.random.seed(n)
+        from colony import simulate as simulate_colony
+        J_colony = []
+        for i in range(N_ed*N_re):
+            J_history, theta_history = simulate_colony(J, N_c=N_c, S=S, rng=rng, d_attract=d_attract, w_attract=w_attract, h_repellant=h_repellant)
+            J_colony.append(np.min(J_history))
+        J_colony_min.append(np.min(J_colony))
+
+        np.random.seed(n)
+        from colony_reproduction import simulate as simulate_colony_re
+        J_colony_re = []
+        for i in range(N_ed):
+            J_history, theta_history = simulate_colony_re(J, N_re=N_re, N_c=N_c, S=S,  rng=rng, d_attract=d_attract, w_attract=w_attract, h_repellant=h_repellant)
+            J_colony_re.append(np.min(J_history))
+        J_colony_re_min.append(np.min(J_colony_re))
+
+        np.random.seed(n)
+        from colony_elimination_dispersal import simulate as simulate_elimination_dispersal
+        J_colony_ed = []
+        J_history, theta_history = simulate_elimination_dispersal(J, N_ed=N_ed, p_ed=p_ed, N_re=N_re, N_c=N_c, S=S,  rng=rng, d_attract=d_attract, w_attract=w_attract, h_repellant=h_repellant)
+        J_colony_ed.append(np.min(J_history))
+        J_colony_ed_min.append(np.min(J_colony_ed))
+
+        np.random.seed(n)
+        theta = rng[0] + np.random.rand(num_seeds*N_ed*N_re*S, p)*(rng[1] - rng[0])
+        J_random = np.apply_along_axis(J, 1, theta)
+        J_random_min.append(np.min(J_random))
+
+    fig = plt.figure()
+    ax = fig.gca()
+    x = ["Single", "Colony", "Reproduction", "Elimination-Dispersal", "Random"]
+    y = [J_single_bacterium_min, J_colony_min, J_colony_re_min, J_colony_ed_min , J_random_min]
+    print(y)
+    ax.violinplot(y)
+    ax.set_xticks(range(1,6))
+    ax.set_xticklabels(x, rotation=45)
+    ax.set_ylabel(r'$J^*$')
+    ax.set_title(title)
+    plt.tight_layout()
+    if filename is None:
+        plt.show()
+    else:
+        plt.savefig(f'{filename}.pdf')
+    plt.close('all')
+
+
 if __name__ == "__main__":
     """
     Used to produce the figures from the presentation.
     """
     from losses import rastrigin as J
 
-    plot_surface(J,
-        filename='presentation/assets/rastrigin',
-        rng=(-5.12,5.12),
-        num=100,
-        cm=cm.rainbow)
+    compare_all(J, filename='presentation/assets/compared')
 
-    np.random.seed(17)
-    evaluate_sb(J,
-        title=r'$p$=2, $c$=0.1, $N_c$=100',
-        filename='presentation/assets/rastrigin',
-        p=2,
-        c=0.1,
-        N_c=100,
-        rng=(-5.12,5.12))
-
-    np.random.seed(17)
-    plot_J_cc(title=r'$d_\mathregular{attract}$=0.1, $w_\mathregular{attract}$=0.2, $h_\mathregular{repellant}$=0.1, $w_\mathregular{repellant}$=10',
-        filename='presentation/assets/swarming',
-        S=3,
-        rng=(-5.12, 5.12),
-        num=100,
-        d_attract=0.1,
-        w_attract=0.2,
-        h_repellant=0.1,
-        w_repellant=10)
-
-    np.random.seed(17)
-    evaluate_col(J,
-        title=r'$S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=0.1, $w_\mathregular{attract}$=0.2, $h_\mathregular{repellant}$=0.1, $w_\mathregular{repellant}$=10',
-        filename='presentation/assets/rastrigin_colony',
-        S=10,
-        p=2,
-        c=0.1,
-        N_c=100,
-        rng=(-5.12,5.12),
-        d_attract=0.1,
-        w_attract=0.2,
-        h_repellant=0.1,
-        w_repellant=10)
-
-    np.random.seed(17)
-    evaluate_col(J,
-        title=r'$S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=100, $w_\mathregular{attract}$=0.01, $h_\mathregular{repellant}$=0.4, $w_\mathregular{repellant}$=0.01',
-        filename='presentation/assets/rastrigin_colony_tuned',
-        S=10,
-        p=2,
-        c=0.1,
-        N_c=100,
-        rng=(-5.12,5.12),
-        d_attract=100,
-        w_attract=0.01,
-        h_repellant=0.4,
-        w_repellant=0.01)
-
-    np.random.seed(17)
-    plot_J_cc(title=r'$S$=3, $p$=2, $d_\mathregular{attract}$=0.1, $w_\mathregular{attract}$=0.2, $h_\mathregular{repellant}$=0.1, $w_\mathregular{repellant}$=10',
-        filename='presentation/assets/swarming_tuned',
-        S=3,
-        rng=(-5.12, 5.12),
-        num=100,
-        d_attract=100,
-        w_attract=0.01,
-        h_repellant=0.4,
-        w_repellant=0.01)
-
-    np.random.seed(17)
-    evaluate_col_re(J,
-        title=r'$N_{re}$=4, $S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=100, $w_\mathregular{attract}$=0.01, $h_\mathregular{repellant}$=0.4, $w_\mathregular{repellant}$=0.01',
-        filename='presentation/assets/rastrigin_colony_re',
-        N_re=4,
-        S=10,
-        p=2,
-        c=0.1,
-        N_c=100,
-        rng=(-5.12,5.12),
-        d_attract=100,
-        w_attract=0.01,
-        h_repellant=0.4,
-        w_repellant=0.01)
-
-    np.random.seed(17)
-    evaluate_col(J,
-        title=r'$S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=100, $w_\mathregular{attract}$=0.01, $h_\mathregular{repellant}$=0.4, $w_\mathregular{repellant}$=0.01',
-        filename='presentation/assets/rastrigin_colony_400',
-        S=10,
-        p=2,
-        c=0.1,
-        N_c=400,
-        rng=(-5.12,5.12),
-        d_attract=100,
-        w_attract=0.01,
-        h_repellant=0.4,
-        w_repellant=0.01)
-
-    np.random.seed(17)
-    evaluate_col_ed(J,
-        title=r'$N_{ed}$=4, $p_{ed}$=0.25, $N_{re}$=4, $S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=100, $w_\mathregular{attract}$=0.01, $h_\mathregular{repellant}$=0.4, $w_\mathregular{repellant}$=0.01',
-        filename='presentation/assets/rastrigin_colony_ed',
-        N_ed=4,
-        p_ed=0.25,
-        N_re=4,
-        S=10,
-        p=2,
-        c=0.1,
-        N_c=100,
-        rng=(-5.12,5.12),
-        d_attract=100,
-        w_attract=0.01,
-        h_repellant=0.4,
-        w_repellant=0.01)
-
-    np.random.seed(17)
-    evaluate_pso(J,
-        title=r'$p$=2, $N$=100, $\mathregular{iter}$=160' '\n' r'$mu$=0.4, $c_1$=0.8, $c_2$=1.0',
-        filename='presentation/assets/pso',
-        p=2,
-        N=100,
-        iter=160,
-        rng=(-5.12, 5.12),
-        mu=0.4,
-        c_1=0.8,
-        c_2=1.0)
+    # plot_surface(J,
+    #     filename='presentation/assets/rastrigin',
+    #     rng=(-5.12,5.12),
+    #     num=100,
+    #     cm=cm.rainbow)
+    #
+    # np.random.seed(17)
+    # evaluate_sb(J,
+    #     title=r'$p$=2, $c$=0.1, $N_c$=100',
+    #     filename='presentation/assets/rastrigin',
+    #     p=2,
+    #     c=0.1,
+    #     N_c=100,
+    #     rng=(-5.12,5.12))
+    #
+    # np.random.seed(17)
+    # plot_J_cc(title=r'$d_\mathregular{attract}$=0.1, $w_\mathregular{attract}$=0.2, $h_\mathregular{repellant}$=0.1, $w_\mathregular{repellant}$=10',
+    #     filename='presentation/assets/swarming',
+    #     S=3,
+    #     rng=(-5.12, 5.12),
+    #     num=100,
+    #     d_attract=0.1,
+    #     w_attract=0.2,
+    #     h_repellant=0.1,
+    #     w_repellant=10)
+    #
+    # np.random.seed(17)
+    # evaluate_col(J,
+    #     title=r'$S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=0.1, $w_\mathregular{attract}$=0.2, $h_\mathregular{repellant}$=0.1, $w_\mathregular{repellant}$=10',
+    #     filename='presentation/assets/rastrigin_colony',
+    #     S=10,
+    #     p=2,
+    #     c=0.1,
+    #     N_c=100,
+    #     rng=(-5.12,5.12),
+    #     d_attract=0.1,
+    #     w_attract=0.2,
+    #     h_repellant=0.1,
+    #     w_repellant=10)
+    #
+    # np.random.seed(17)
+    # evaluate_col(J,
+    #     title=r'$S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=100, $w_\mathregular{attract}$=0.01, $h_\mathregular{repellant}$=0.4, $w_\mathregular{repellant}$=0.01',
+    #     filename='presentation/assets/rastrigin_colony_tuned',
+    #     S=10,
+    #     p=2,
+    #     c=0.1,
+    #     N_c=100,
+    #     rng=(-5.12,5.12),
+    #     d_attract=100,
+    #     w_attract=0.01,
+    #     h_repellant=0.4,
+    #     w_repellant=0.01)
+    #
+    # np.random.seed(17)
+    # plot_J_cc(title=r'$S$=3, $p$=2, $d_\mathregular{attract}$=0.1, $w_\mathregular{attract}$=0.2, $h_\mathregular{repellant}$=0.1, $w_\mathregular{repellant}$=10',
+    #     filename='presentation/assets/swarming_tuned',
+    #     S=3,
+    #     rng=(-5.12, 5.12),
+    #     num=100,
+    #     d_attract=100,
+    #     w_attract=0.01,
+    #     h_repellant=0.4,
+    #     w_repellant=0.01)
+    #
+    # np.random.seed(17)
+    # evaluate_col_re(J,
+    #     title=r'$N_{re}$=4, $S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=100, $w_\mathregular{attract}$=0.01, $h_\mathregular{repellant}$=0.4, $w_\mathregular{repellant}$=0.01',
+    #     filename='presentation/assets/rastrigin_colony_re',
+    #     N_re=4,
+    #     S=10,
+    #     p=2,
+    #     c=0.1,
+    #     N_c=100,
+    #     rng=(-5.12,5.12),
+    #     d_attract=100,
+    #     w_attract=0.01,
+    #     h_repellant=0.4,
+    #     w_repellant=0.01)
+    #
+    # np.random.seed(17)
+    # evaluate_col(J,
+    #     title=r'$S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=100, $w_\mathregular{attract}$=0.01, $h_\mathregular{repellant}$=0.4, $w_\mathregular{repellant}$=0.01',
+    #     filename='presentation/assets/rastrigin_colony_400',
+    #     S=10,
+    #     p=2,
+    #     c=0.1,
+    #     N_c=400,
+    #     rng=(-5.12,5.12),
+    #     d_attract=100,
+    #     w_attract=0.01,
+    #     h_repellant=0.4,
+    #     w_repellant=0.01)
+    #
+    # np.random.seed(17)
+    # evaluate_col_ed(J,
+    #     title=r'$N_{ed}$=4, $p_{ed}$=0.25, $N_{re}$=4, $S$=10, $p$=2, $c$=0.1, $N_c$=100' '\n' r'$d_\mathregular{attract}$=100, $w_\mathregular{attract}$=0.01, $h_\mathregular{repellant}$=0.4, $w_\mathregular{repellant}$=0.01',
+    #     filename='presentation/assets/rastrigin_colony_ed',
+    #     N_ed=4,
+    #     p_ed=0.25,
+    #     N_re=4,
+    #     S=10,
+    #     p=2,
+    #     c=0.1,
+    #     N_c=100,
+    #     rng=(-5.12,5.12),
+    #     d_attract=100,
+    #     w_attract=0.01,
+    #     h_repellant=0.4,
+    #     w_repellant=0.01)
+    #
+    # np.random.seed(17)
+    # evaluate_pso(J,
+    #     title=r'$p$=2, $N$=100, $\mathregular{iter}$=160' '\n' r'$mu$=0.4, $c_1$=0.8, $c_2$=1.0',
+    #     filename='presentation/assets/pso',
+    #     p=2,
+    #     N=100,
+    #     iter=160,
+    #     rng=(-5.12, 5.12),
+    #     mu=0.4,
+    #     c_1=0.8,
+    #     c_2=1.0)
